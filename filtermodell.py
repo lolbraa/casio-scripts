33c86b7e751b55cd1e7a11ae9db5de002f68f830
Kristoffer Braa <kristoffer@krisslaptoppop.barn-banjo.ts.net>
2024-10-10 13:52:57 +0200

    Implementere KRC

--- filtermodell.py
+++ filtermodell.py
@@ -7,19 +7,12 @@ def cosh(x):
     return (math.exp(x) + math.exp(-x)) / 2
 
 
-def finnApproksimasjon():
+def finnApproksimasjon(passType):
     print("Dette programmet gir deg\nverdiene til ulike\nfilterapproksimasjoner.")
-    while True:
-        print("Velg filtertype:")
-        passType = input("LP eller HP? (1/2): ").strip().upper()
-        if passType in ['1', '2']:
-            break
-        else:
-            print("Ugyldig valg.\nVelg 'B' for Butterworth\neller 'C' for Chebyshev.")
 
     while True:
         print("Velg approksimasjonsmodell:")
-        approksimasjonsType = input("Butt. eller Cheb.? (1/2): ").strip().upper()
+        approksimasjonsType = input("Butt. eller Cheb.?\n(1/2): ").strip().upper()
         if approksimasjonsType in ['1', '2']:
             break
         else:
@@ -60,10 +53,77 @@ def finnApproksimasjon():
     print("Bruk disse verdiene til å finne riktig verdier i tabellen.")
 
 
+def finnKRC_LP():
+
+    w0 = float(input("Skriv inn w0: "))
+    Q = float(input("Skriv inn Q: "))
+    R = float(input("Skriv inn R: "))
+    
+    C2_temp = 1 / (2 * R * w0 * Q)
+    print("C2_temp: ", C2_temp)
+    
+    C2 = float(input("Oppgi ønsket C2 i pF (10^-12): ")) * 10 ** -12
+    C1_temp = 4 * Q ** 2 * C2
+    print("C1_temp: ", C1_temp)
+    
+    C1 = float(input("Oppgi ønsket C1: "))
+    n = C1 / C2
+    k = n / (2 * Q ** 2) - 1
+    m = k + math.sqrt(k ** 2 - 1)
+    R2 = 1 / (math.sqrt(m * n) * w0 * C2)
+    R1 = m * R2
+    
+    print("R1: ", R1)
+    print("R2: ", R2)
+
+def finnKRC(passType):
+    print("Finn verdier for et KRC-filter,\nogså kalt Sallen-Key-filter.\nOBS: Ingen forsterkning, K.")
+    
+    if passType == '1':
+        # spør etter orden n
+        while True:
+            try:
+                n = int(input("Skriv inn orden n: "))
+                if n > 1:
+                    break
+                else:
+                    print("Orden n må være et heltall større enn 1.")
+            except ValueError:
+                print("Ugyldig input. Vennligst skriv inn et heltall.")
+        finnKRC_LP()
+    else:
+        print("Ugyldig valg. Velg '1' for LP.")
 
 
-finnApproksimasjon()
 
+def main():
+    # spørr hvilken handling brukeren vil utføre
+    while True:
+        print("Velg handling:")
+        valg = input("1: Finn verdier for en approksimasjons-modell\n2: Finn verdier for et KRC-filter\n3: Først 1 så 2\n(1/2/3): ").strip()
+        if valg in ['1', '2', '3']:
+            break
+        else:
+            print("Ugyldig valg. Vennligst velg 1, 2 eller 3.")
+
+    # spør om filtertype, lavpass eller høypass (eller båndpass)
+    while True:
+        print("Velg filtertype:")
+        passType = input("LP eller HP?\n(1/2): ").strip().upper()
+        if passType in ['1', '2']:
+            break
+        else:
+            print("Ugyldig valg.\nVelg '1' for Lavpass\neller '2' for Høypass.")
+
+    if valg == '1':
+        finnApproksimasjon(passType)
+    elif valg == '2':
+        finnKRC(passType)
+    elif valg == '3':
+        finnApproksimasjon(passType)
+        finnKRC(passType)
+
+main()
 
 
 
